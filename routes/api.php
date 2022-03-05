28c9c7aaae46f1fec910ab4b11cab546b3acf81d
Rika widya astuti <rhicaicha3@gmail.com>
2022-03-05 17:02:32 +0700

    push

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,7 @@ use PhpParser\Node\Expr\PostDec;
 */
 Route::post('/register', [\App\Http\Controllers\RegisterController::class, 'register']);
 
-Route::post('/login', [\App\Http\Controllers\LoginController::class, 'login']);
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+
 });
